81da04cda7e376313165d601df5b0422c21a84ed
kevin.chavez <ami896717@gmail.com>
2022-01-19 19:12:32 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let kilometros = prompt ("ingresa la cantidad de kilometros diarios")
+let kilometrosExtra = prompt ("agrega quilometros a tu rutina")
+
+while (kilometros <= 5) {
+    alert ("estas en perfectas condiciones fisicas")
+    kilometros ++;
+
+    if (kilometros >5) {
+        kilometrosExtra = kilometrosExtra + kilometros;
+    }
+}
+
+
