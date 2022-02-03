757317e039a43e5c4a5d79772e839ce894b0d7ea
kevin.chavez <ami896717@gmail.com>
2022-02-02 22:00:27 -0300

    funciones del orden superior

--- js/main.js
+++ js/main.js
@@ -15,60 +15,50 @@ alert ("con esos kilometros tu salud y condicion fisica sera optima"); */
 
 /* ----ARRAYS---- */
 
-const bicicletas = [
-    {tipo:"bmx", precio:"3000"},
-    {tipo:"kids", precio:"2000"},
-    {tipo:"mtb", precio:"5000"},
-    {tipo:"ruta", precio:"9000"}
+const productos = [
+    {tipo:"bmx", precio: 3000},
+    {tipo:"kids", precio: 2000},
+    {tipo:"mtb", precio: 5000},
+    {tipo:"ruta", precio: 9000},
+    {tipo:"asiento", precio: 300},
+    {tipo:"baliza", precio: 100},
+    {tipo:"freno", precio: 200},
+    {tipo:"inflador", precio: 250}
 ];
 
-function cadaBici (arr, fn) {
+function cadaProducto (arr, fn) {
     for (const el of arr) {
         fn(el)
     }
 }
 
-cadaBici (bicicletas, console.log)
+cadaProducto (productos, console.log)
 
-const accesorios = [
-    {tipo:"asiento", precio:"300"},
-    {tipo:"baliza", precio:"100"},
-    {tipo:"freno", precio:"200"},
-    {tipo:"inflador", precio:"250"},
-    {tipo:"kitPurgado", precio:"350"},
-    {tipo:"linga", precio:"150"},
-    {tipo:"liquidoPPinchaduras", precio:"500"},
-    {tipo:"luzDelantera", precio:"150"},
-    {tipo:"monoplato", precio:"600"},
-    {tipo:"pedalBMX", precio:"400"},
-    {tipo:"pedalSportrace", precio:"450"},
-    {tipo:"pedalMtbSportrace", precio:"500"},
-    {tipo:"pedalSportace2", precio:"400"},
-    {tipo:"pedalTopmega", precio:"800"},
-    {tipo:"plataformaBloqueo", precio:"480"},
-    {tipo:"portaBicicleta", precio:"1000"},
-    {tipo:"portaCaramañola", precio:"340"},
-    {tipo:"portaCelular", precio:"480"},
-    {tipo:"protector", precio:"300"},
-    {tipo:"puñoRanser", precio:"200"},
-    {tipo:"ruedaCambio", precio:"650"},
-    {tipo:"sensorVelocidad", precio:"1050"},
-    {tipo:"sillaPortaMuñeca", precio:"800"},
-    {tipo:"soporte", precio:"850"},
-    {tipo:"soporteFrontal", precio:"880"},
-    {tipo:"valvula", precio:"500"}
-]
+const compra = [];
+let cantidad = prompt("ingresa la cantidad depruductos ");
 
-function cadaAccesorio (arr, fn) {
-    for (const el of arr) {
-        fn(el)
+do {
+    let nombre = prompt("ingresa el nombre del producto"),
+    precio = parseInt (prompt ("ingresa el precio"));
+
+    const producto = {
+        nombre : nombre,
+        precio : precio
     }
-}
 
-cadaAccesorio (accesorios, console.log)
+    compra.push(producto);
+    console.log(compra.length);
+
+} while (compra.length != cantidad);
+
+const total = compra.reduce ((acc, el) => acc + el.precio, 0)
+alert ("el total de tu compra es " + total )
 
 
 /* ---DISPOSITIVO INTERACTIVO--- */
+
+alert ("a continuacion continuaremos con el pago de tu compra")
+
 function apellidoNombre (){
 
     let nombreYapellido = prompt ("ingrsa tu nombre y apellido");
