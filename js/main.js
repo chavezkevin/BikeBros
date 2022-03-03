5f9c8ba172df139e02b9a4eea1a7f11f27651d59
kevin.chavez <ami896717@gmail.com>
2022-03-03 16:29:39 -0300

    mostrar productos atravez de Json

--- js/main.js
+++ js/main.js
@@ -1,86 +1,78 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
 
     // Variables
-    const baseDeDatos = [
+    let baseDeDatos = [
         {
             id: 1,
             nombre: 'BICICLETA BMX DIOMENES',
-            precio: 49.759,
+            precio: 49759,
             imagen: './img/img-BMX/bmxDiomenes.png',
             descripcion: 'Una bicicleta para los que viven buscando nuevos desafíos, andar con estilo disfrutando al extremo. Ágil y resistente para que puedas realizar maniobras y saltos, demostrándote que podes superarte cada día.'
         },
         {
             id: 2,
             nombre: 'BICICLETA CROSSBOY-R20',
-            precio: 32.495,
+            precio: 32495,
             imagen: './img/img-KIDS/crossboy.png',
             descripcion: 'pensada y diseñada para los niños con mucha energia y preparados para la aventura.'
         },
         {
             id: 3,
-            nombre: 'BICICLETA CROSSBOY-R12',
-            precio: 24.915,
-            imagen: './img/img-KIDS/crossboyR12.png',
-            descripcion: 'para los mas chiquitos, lista para soportar toda la energia.'
-        },
-        {
-            id: 4,
             nombre: 'BICICLETA CROSSBOY-R16',
-            precio: 25.915,
+            precio: 25915,
             imagen: './img/img-KIDS/crossboyR16.png',
             descripcion: 'bicicleta para esos niños dispuestor a ganar.'
         },
         {
-            id: 5,
+            id: 4,
             nombre: 'BICICLETA FLEXYGIRL-R20',
-            precio: 32.495,
+            precio: 32495,
             imagen: './img/img-KIDS/flexygirl.png',
             descripcion: 'bicicleta para esas niñas aventureras, incorporado con un cesto para pasear a sus juguetes.'
         },
         {
-            id: 6,
+            id: 5,
             nombre: 'BICICLETA MTB FlAMINGO',
-            precio: 55.099,
+            precio: 55099,
             imagen: './img/img-MTB/mtbflamingo.png',
-            descripcion: 'Esta bicicleta se adapta a vos, pensada en tu comodidad. Con un diseño y una geometría que te dará mayor confort y maniobrabilidad. Los frenos a disco te brindan esa seguridad y precisión que necesitas en tus recorridos'
+            descripcion: 'Esta bicicleta se adapta a vos, pensada en tu comodidad. Con un diseño y una geometría que te dará mayor confort y maniobrabilidad. Los frenos a disco te brindan esa seguridad y precisión que necesitas en tus recorridos.'
         },
         {
-            id: 7,
+            id: 6,
             nombre: 'BICICLETA MTB NEPTUNE-R26',
-            precio: 53.690,
+            precio: 53690,
             imagen: './img/img-MTB/mtbneptune.png',
             descripcion: 'Esta bicicleta es para los que se divierten viviendo nuevas aventuras. Versátil y estable, tu compañera para moverte en la ciudad y llevar una vida más deportiva.'
         },
         {
-            id: 8,
+            id: 7,
             nombre: 'BICICLETA MTB REGAL',
-            precio: 58.957,
+            precio: 58957,
             imagen: './img/img-MTB/mtbregal.png',
-            descripcion: 'Esta bicicleta se adapta a cualquier situacion, con sus 21 velocidades no tendras problemas de surcar cualquier obstaculo'
+            descripcion: 'Esta bicicleta se adapta a cualquier situacion, con sus 21 velocidades no tendras problemas de surcar cualquier obstaculo.'
+        },
+        {
+            id: 8,
+            nombre: 'BICICLETA MTB SUNSHINE-R29',
+            precio: 73605,
+            imagen: './img/img-MTB/mtbsunshine.png',
+            descripcion: 'Esta bicicleta fue diseñada para circular en cualquier clase de terreno, gracias a su talle S no habra problemas de crusar del asfalto a la tierra.'
         },
         {
             id: 9,
-            nombre: 'BICICLETA MTB SUNSHINE-R29',
-            precio: 73.605,
-            imagen: './img/img-MTB/mtbsunshine.png',
-            descripcion: 'Esta bicicleta fue diseñada para circular en cualquier clase de terreno, gracias a su talle S no habra problemas de crusar del asfalto a la tierra'
-        },
-        {
-            id: 10,
             nombre: 'BICICLETA MTB THOR LITE-R29',
-            precio: 83.837,
+            precio: 83837,
             imagen: './img/img-MTB/mtbthor.png',
             descripcion: 'Esta bicicleta es la mas completa del mercado, incluye 24 velocidades y su talle S genera mucha mas comodidad al circular, podes encontrarla en colores negro o amarillo.'
         },
         {
-            id: 11,
+            id: 10,
             nombre: 'Bicicleta RUTA Harrier',
-            precio: 189.265,
+            precio: 189265,
             imagen: './img/img-RUTA/rutaharrier.png',
-            descripcion: 'Tecnología pensada en tu necesidad de llegar más lejos y de superarte en cada salida. Con la comodidad que buscas para alargar tus recorridos y alcanzar nuevas metas. Más rápida y ligera para que vivas la velocidad con todos tus sentidos'
+            descripcion: 'Tecnología pensada en tu necesidad de llegar más lejos y de superarte en cada salida. Con la comodidad que buscas para alargar tus recorridos y alcanzar nuevas metas. Más rápida y ligera para que vivas la velocidad con todos tus sentidos.'
         }
-
-    ];
+    ]
 
     let carrito = [];
     const divisa = '$';
@@ -95,160 +87,235 @@ document.addEventListener('DOMContentLoaded', () => {
     /**
     * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
     */
-    function renderizarProductos() {
-        baseDeDatos.forEach((info) => {
-            // Estructura
-            const miNodo = document.createElement('div');
-            miNodo.classList.add('card', 'col-sm-5', 'm-1');
-            // Body
-            const miNodoCardBody = document.createElement('div');
-            miNodoCardBody.classList.add('card-body', 'd-flex', 'flex-column', 'justify-content-between');
-            // Titulo
-            const miNodoTitle = document.createElement('h3');
-            miNodoTitle.classList.add('card-title');
-            miNodoTitle.textContent = info.nombre;
 
-            const miNododescripcion = document.createElement('p')
-            miNododescripcion.classList.add('card-text')
-            miNododescripcion.textContent = info.descripcion
-            // Imagen
-            const miNodoImagen = document.createElement('img');
-            miNodoImagen.classList.add('img-fluid');
-            miNodoImagen.setAttribute('src', info.imagen);
-            // Precio
-            const miNodoPrecio = document.createElement('p');
-            miNodoPrecio.classList.add('card-text');
-            miNodoPrecio.textContent = `${info.precio}${divisa}`;
-            // Boton 
-            const miNodoBoton = document.createElement('button');
-            miNodoBoton.classList.add('btn', 'btn-primary');
-            miNodoBoton.textContent = '+';
-            miNodoBoton.setAttribute('marcador', info.id);
-            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
-            // Insertamos
-            miNodoCardBody.appendChild(miNodoImagen);
-            miNodoCardBody.appendChild(miNodoTitle);
-            miNodoCardBody.appendChild(miNododescripcion);
-            miNodoCardBody.appendChild(miNodoPrecio);
-            miNodoCardBody.appendChild(miNodoBoton);
-            miNodo.appendChild(miNodoCardBody);
-            DOMitems.appendChild(miNodo);
+
+/*function renderizarProductos() {
+    baseDeDatos.forEach((info)=>{
+
+
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4', 'm-1', 'text-white', 'bg-secondary');
+
+
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body', 'd-flex', 'flex-column', 'justify-content-between');
+
+
+        // Titulo
+        const miNodoTitle = document.createElement('h3');
+
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+
+        const miNododescripcion = document.createElement('p')
+        miNododescripcion.classList.add('card-text')
+        miNododescripcion.textContent = info.descripcion
+
+
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid', 'rounded-3');
+        miNodoImagen.setAttribute('src', info.imagen);
+
+
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+
+
+        // Boton 
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito)
+        miNodoBoton.addEventListener('click', () => {
+            swal.fire({
+                html: `<div class="alert alert-success d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                <div>
+                <h2>ACABAS DE AGREGAR UN PRPODUCTO A TU CARRITO</h2>
+                </div>
+                </div>`,
+                icon: 'success'
+            })
         });
-    }
 
-    /**
-    * Evento para añadir un producto al carrito de la compra
-    */
-    function anyadirProductoAlCarrito(evento) {
-        // Anyadimos el Nodo a nuestro carrito
-        carrito.push(evento.target.getAttribute('marcador'))
-        // Actualizamos el carrito 
-        renderizarCarrito();
-        // Actualizamos el LocalStorage
-        guardarCarritoEnLocalStorage();
-    }
 
-    /**
-    * Dibuja todos los productos guardados en el carrito
-    */
-    function renderizarCarrito() {
-        // Vaciamos todo el html
-        DOMcarrito.textContent = '';
-        // Quitamos los duplicados
-        const carritoSinDuplicados = [...new Set(carrito)];
-        // Generamos los Nodos a partir de carrito
-        carritoSinDuplicados.forEach((item) => {
-            // Obtenemos el item que necesitamos de la variable base de datos
-            const miItem = baseDeDatos.filter((itemBaseDatos) => {
-                // ¿Coincide las id? Solo puede existir un caso
-                return itemBaseDatos.id === parseInt(item);
-            });
-            // Cuenta el número de veces que se repite el producto
-            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-                return itemId === item ? total += 1 : total;
-            }, 0);
-            // Creamos el nodo del item del carrito
-            const miNodo = document.createElement('li');
-            miNodo.classList.add('list-group-item', 'text-right', 'mx-2', 'item');
-            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-            // Boton de borrar
-            const miBoton = document.createElement('button');
-            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-            miBoton.textContent = 'X';
-            miBoton.style.marginLeft = '1rem';
-            miBoton.dataset.item = item;
-            miBoton.addEventListener('click', borrarItemCarrito);
-            // Mezclamos nodos
-            miNodo.appendChild(miBoton);
-            DOMcarrito.appendChild(miNodo);
-        });
-        // Renderizamos el precio total en el HTML
-        DOMtotal.textContent = calcularTotal();
-    }
 
-    /**
-    * Evento para borrar un elemento del carrito
-    */
-    function borrarItemCarrito(evento) {
-        // Obtenemos el producto ID que hay en el boton pulsado
-        const id = evento.target.dataset.item;
-        // Borramos todos los productos
-        carrito = carrito.filter((carritoId) => {
-            return carritoId !== id;
-        });
-        // volvemos a renderizar
-        renderizarCarrito();
-        // Actualizamos el LocalStorage
-        guardarCarritoEnLocalStorage();
+        // Insertamos
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNododescripcion);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
+}
 
-    }
+/**
+* Evento para añadir un producto al carrito de la compra
+*/
+/*function anyadirProductoAlCarrito(evento) {
 
-    /**
-     * Calcula el precio total teniendo en cuenta los productos repetidos
-     */
-    function calcularTotal() {
-        // Recorremos el array del carrito 
-        return carrito.reduce((total, item) => {
-            // De cada elemento obtenemos su precio
-            const miItem = baseDeDatos.filter((itemBaseDatos) => {
-                return itemBaseDatos.id === parseInt(item);
-            });
-            // Los sumamos al total
-            return total + miItem[0].precio;
-        }, 0).toFixed(2);
-    }
-
-    /**
-    * Varia el carrito y vuelve a dibujarlo
-    */
-    function vaciarCarrito() {
-        // Limpiamos los productos guardados
-        carrito = [];
-        // Renderizamos los cambios
-        renderizarCarrito();
-        // Borra LocalStorage
-        localStorage.clear();
-
-    }
-
-    function guardarCarritoEnLocalStorage () {
-        miLocalStorage.setItem('carrito', JSON.stringify(carrito));
-    }
-
-    function cargarCarritoDeLocalStorage () {
-        // ¿Existe un carrito previo guardado en LocalStorage?
-        if (miLocalStorage.getItem('carrito') !== null) {
-            // Carga la información
-            carrito = JSON.parse(miLocalStorage.getItem('carrito'));
-        }
-    }
-
-    // Eventos
-    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
-
-    // Inicio
-    cargarCarritoDeLocalStorage();
-    renderizarProductos();
+    // Anyadimos el Nodo a nuestro carrito
+    carrito.push(evento.target.getAttribute('marcador'))
+    // Actualizamos el carrito 
     renderizarCarrito();
-});
+    // Actualizamos el LocalStorage
+    guardarCarritoEnLocalStorage();
+}
+
+/**
+* Dibuja todos los productos guardados en el carrito
+*/
+/*function renderizarCarrito() {
+    // Vaciamos todo el html
+    DOMcarrito.textContent = '';
+    // Quitamos los duplicados
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Generamos los Nodos a partir de carrito
+    carritoSinDuplicados.forEach((item) => {
+        // Obtenemos el item que necesitamos de la variable base de datos
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            // ¿Coincide las id? Solo puede existir un caso
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Cuenta el número de veces que se repite el producto
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        // Creamos el nodo del item del carrito
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2', 'item');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+        // Boton de borrar
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+
+        miBoton.addEventListener('click', borrarItemCarrito,)
+        miBoton.addEventListener('click', () => {
+            swal.fire({
+                html: `<div class="alert alert-success d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                <div>
+                <h2>ESTE PRODUCTO A SIDO ELIMINADO</h2>
+                </div>
+                </div>`,
+                icon: 'succes'
+            })
+        })
+
+
+        // Mezclamos nodos
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+    // Renderizamos el precio total en el HTML
+    DOMtotal.textContent = calcularTotal();
+}
+
+/**
+* Evento para borrar un elemento del carrito
+*/
+/*function borrarItemCarrito(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Borramos todos los productos
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    // volvemos a renderizar
+    renderizarCarrito();
+    // Actualizamos el LocalStorage
+    guardarCarritoEnLocalStorage();
+
+}
+
+/**
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+/*function calcularTotal() {
+    // Recorremos el array del carrito 
+    return carrito.reduce((total, item) => {
+        // De cada elemento obtenemos su precio
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumamos al total
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+}
+
+/**
+* Varia el carrito y vuelve a dibujarlo
+*/
+/*function vaciarCarrito() {
+    // Limpiamos los productos guardados
+    carrito = [];
+    // Renderizamos los cambios
+    renderizarCarrito();
+    // Borra LocalStorage
+    localStorage.clear();
+
+}
+
+function guardarCarritoEnLocalStorage() {
+    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function cargarCarritoDeLocalStorage() {
+    // ¿Existe un carrito previo guardado en LocalStorage?
+    if (miLocalStorage.getItem('carrito') !== null) {
+        // Carga la información
+        carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+    }
+}
+
+// Eventos
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+// Inicio
+cargarCarritoDeLocalStorage();
+renderizarProductos();
+renderizarCarrito();
+});*/
+
+const lista = document.getElementById("lista")
+
+const url = "./js/productos.json"
+
+
+fetch(url)
+
+    .then(respuesta => respuesta.json())
+    .then(producto => {
+        producto.forEach(elemento => {
+            const li = document.createElement("li");
+
+            li.innerHTML = `
+        <div class="card bg-info border-dark" style="width: 18rem;">
+            <img src="${elemento.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-title">${elemento.nombre}</h2>
+                <p class="card-text">${elemento.descripcion}</p>
+                <p class="card-text">${elemento.precio}</p>
+            </div>
+        </div>
+        `
+            lista.append(li)
+        })
+    })
+
+
+
+
